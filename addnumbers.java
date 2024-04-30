41c5830b3fb91b0f62e8b71e0496ffb5bac8dc9e
Aryan4607 <124121620+Aryan4607@users.noreply.github.com>
2024-05-01 02:07:44 +0530

    Add files via upload

---
+++ addnumbers.java
@@ -0,0 +1,11 @@
+public class addnumbers {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+
+        int sum = num1 + num2;
+
+        System.out.println("sum" + sum);
+
+    }
+}
